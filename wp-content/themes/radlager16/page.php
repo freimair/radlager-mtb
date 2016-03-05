7c46e683ed6968f42c8f41e86e6d89274c24098f
Florian Reimair <florian.reimair@itdevas.at>
2016-03-05 12:45:41 +0100

    page title filters displayed categories

--- wp-content/themes/radlager16/page.php
+++ wp-content/themes/radlager16/page.php
@@ -16,19 +16,29 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<?php
+
+		// create new loop based on the categories named in the title of the post
+		// - but first make sure nothing can go sideways
+		$categories = $post->post_title;
+		if(preg_match("/^[a-zA-Z0-9,]+$/", $categories))
+			$categories = $post->post_title;
+		else
+			// do not show anything in case of an error
+			$categories = "category_that_d0es_n0t_exist_almost_100_perZent";
+
+		// - now start the query
+		$query = new WP_Query( array ('category_name' => $categories ) );
+
 		// Start the loop.
-		while ( have_posts() ) : the_post();
+		while ( $query->have_posts() ) : $query->the_post();
 
 			// Include the page content template.
 			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-
 			// End of the loop.
 		endwhile;
+
+		wp_reset_postdata(); //resetting the post query
 		?>
 
 	</main><!-- .site-main -->
