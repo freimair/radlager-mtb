cba259b4b7a2396a6cd389cf64cefd26f7a6b491
Florian Reimair <florian.reimair@itdevas.at>
2016-03-23 19:56:04 +0100

    cancel post creation

--- wp-content/plugins/frontend-create-posts/frontend-create-posts.php
+++ wp-content/plugins/frontend-create-posts/frontend-create-posts.php
@@ -139,15 +139,16 @@ function frontend_edit_posts_form($post_id, $categories, $caption) {
 jQuery(document).ready(function(){
 	jQuery("input#edit-post").click(function(e){
 		e.preventDefault();
-		var post_id = jQuery(this).attr("data-post_id");
-		var categories = JSON.parse(jQuery(this).attr("data-categories"));
+		if(jQuery("div#edit-post-form").is(':empty')) {
+			var post_id = jQuery(this).attr("data-post_id");
+			var categories = JSON.parse(jQuery(this).attr("data-categories"));
 
-		jQuery('div#edit-post-form').load('/wp-admin/admin-ajax.php', {"action" : "frontend_edit_post_form", "post_id" : post_id, "category_ids" : categories}, function() {
-			// trigger setup for all ACF fields in case there are some that need initializing
-			jQuery(document).trigger('acf/setup_fields', jQuery('div#edit-post-form'));
+			jQuery('div#edit-post-form').load('/wp-admin/admin-ajax.php', {"action" : "frontend_edit_post_form", "post_id" : post_id, "category_ids" : categories}, function() {
+				// trigger setup for all ACF fields in case there are some that need initializing
+				jQuery(document).trigger('acf/setup_fields', jQuery('div#edit-post-form'));
 
-			// hook the submit button in order to do an ajax submit
-			jQuery('div#edit-post-form input#submit').click(function(e) {
+				// hook the submit button in order to do an ajax submit
+				jQuery('div#edit-post-form input#submit').click(function(e) {
 					e.preventDefault();
 
 					// the new and shiny editor does some woodoo with iframes and stuff. Hence,
@@ -169,7 +170,16 @@ jQuery(document).ready(function(){
 						}
 					});
 				});
+
+				// replace button action
+				jQuery("input#edit-post").val("nein, sorry, doch nicht...");
 			});
+		} else {
+			jQuery("div#edit-post-form").empty();
+			tinyMCE.editors=[];
+			jQuery(".mce-container").remove();
+			jQuery("input#edit-post").val("<?php echo $caption; ?>");
+		}
 	});
 });
 </script>
