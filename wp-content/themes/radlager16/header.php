8ec3588a3b9cfe4e7c3482cadbd4e964fc94dd18
Florian Reimair <florian.reimair@itdevas.at>
2016-06-09 17:38:31 +0200

    added google analytics

--- wp-content/themes/radlager16/header.php
+++ wp-content/themes/radlager16/header.php
@@ -14,11 +14,25 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="google-site-verification" content="nF70r7qfX7zntMZneQKWqzGiPuAPYNYIrFkaf_tuiUE" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
 	<?php wp_head(); ?>
+	<script type="text/javascript" src="<?php echo get_site_url(); ?>/wp-content/themes/radlager16/js/header-functions.js"></script>
+
+	<!-- google analytcs tracking code -->
+
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-26722473-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
