9be5a72c6a62e9e90e51fe6bfe4205c48ea3074f
Florian Reimair <florian.reimair@itdevas.at>
2016-06-21 19:54:53 +0200

    created 'subsite' via template

---
+++ wp-content/themes/radlager16/page-area_one.php
@@ -0,0 +1,21 @@
+<?php
+/**
+    Template Name: Area One
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+
+	get_header("area_one");
+?>
+<div id="primary" class="content-area">
+<?php
+	include('page_commons.php');
+ ?>
