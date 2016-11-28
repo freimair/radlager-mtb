bb3c72dafc9c129126de1e1582b1657226614ec7
Florian Reimair <florian.reimair@itdevas.at>
2016-11-28 19:20:06 +0100

    added cancel search button

--- wp-content/themes/radlager16/page_commons.php
+++ wp-content/themes/radlager16/page_commons.php
@@ -199,6 +199,7 @@ jQuery('#up').click(function() {
 		?>
 			<li class="filter">
 				<input id="searchbox" type="text" name="searchterm" value="">
+				<input id="clearsearch" type="button" value="X" onclick="jQuery('#searchbox').val('')">
 			</li>
 		</ul>
 
