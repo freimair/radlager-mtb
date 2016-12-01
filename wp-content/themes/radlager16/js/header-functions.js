e24af0bc293b113103d1f14335048cda27e4385d
zaila99 <gernot_winkler@gmx.at>
2016-12-01 14:51:26 +0100

    make on-scoll js event for banner/logo

--- wp-content/themes/radlager16/js/header-functions.js
+++ wp-content/themes/radlager16/js/header-functions.js
@@ -1,35 +1,14 @@
-
-function header() {
-	
-	jQuery(window).scroll(function() {
-
-						  
-		if ( jQuery(document).height() >= 1500) {
-           
-        
-						  
-						 
+jQuery(window).scroll(function() {
+   
+		 var scrollPos = jQuery(window).scrollTop();
 		
-		scrollPos = jQuery(window).scrollTop();
-		
-		if (scrollPos >= 1) {
-			
+		if (scrollPos >= 1) {	
 			jQuery(".banner").css({"margin-top":"-337px"});
-			jQuery(".RL-Logo_oben").css({"display":"block","opacity":"1"});
-			
-			
-		} else {
-			
-			jQuery(".banner").css({"margin-top":"134px"});
-			jQuery(".RL-Logo_oben").css({"opacity":"0"});
-			
-		}
-	
+			jQuery(".RL-Logo_oben").css({"display":"block","opacity":"1"});	
+	}
+	else
+	{
+		jQuery(".banner").css({"margin-top":"75px"});
+		jQuery(".RL-Logo_oben").css({"display":"block","opacity":"0"});
 	}
 	});
-	
-	
-	
-
-}
-
