a158ba192a9abe5a05d4da86287063c9e9067111
Gernot Winkler <gernot_winkler@gmx.at>
2016-12-07 01:46:59 +0100

    New Banner

--- wp-content/themes/radlager16/header_commons.php
+++ wp-content/themes/radlager16/header_commons.php
@@ -88,19 +88,26 @@
 				
 				
 				<div class="banner">
-					<div class="RL-Logo_oben">
-					</div>
+					<div class="RL-Logo_oben"></div>
 					
 				
-					<div class="RL-Logo">
-						
-					</div>
+						<div class="Logos">
+								<div class="enzi-logo" ><img src="<?php echo get_site_url(); ?>/wp-content/themes/radlager16/Pictures/Logos/enzi-logo.png"></div>
+								<div class="rl-logo"><img  src="<?php echo get_site_url(); ?>/wp-content/themes/radlager16/Pictures/Logos/RLgruen.png"></div>
+							<div class="ao-logo" ><a href="<?php echo get_site_url() ; ?>/index.php/area_one_medien/">
+								<img src="<?php echo get_site_url(); ?>/wp-content/themes/radlager16/Pictures/Logos/hiergehtszurareaone.png">
+							</a>
+						</div>
+				</div>
 
-					<a target="_blank" href="https://www.facebook.com/RadlagerMTB">
+					
 					<div class="FB-Button">
+						<a target="_blank" href="https://www.facebook.com/RadlagerMTB">
 						<img style="margin-top:-3px;" src="<?php echo get_site_url(); ?>/wp-content/themes/radlager16/Pictures/SVG/facebook-icon-header.png">&nbsp;&nbsp;Radlager auf Facebook
+						</a>
 					</div>
-					</a>
+					
+		<!--		
 				
 					<div class="nav_to_subsite" ><a href="<?php 
 // TODO that is nasty. fix that someday!
@@ -108,6 +115,9 @@ if("area_one" == $navigation)
 	echo get_site_url() ;
 else
 	echo get_site_url() . "/index.php/area_one_medien/"; ?>" style="display: block; width: 100%; height: 100%"></a></div>
+					
+					
+				-->
 				</div>
 				
 				
