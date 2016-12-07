a90ee78f1c96099f8913eebbd28bbdc1ca02a1e7
Gernot Winkler <gernot_winkler@gmx.at>
2016-12-07 12:56:38 +0100

    style footer and put payment notification into it

--- wp-content/themes/radlager16/page.php
+++ wp-content/themes/radlager16/page.php
@@ -16,13 +16,6 @@ get_header();
  ?>
 
 <div id="primary" class="content-area">
-<?php
-	if('open' === get_user_meta(get_current_user_id(), 'radlager_membership_fee_status', true) && is_user_logged_in()) :
-?>
-<div id="payment_notification"><strong>Zahl endlich!</strong> Lt. unseren Aufzeichnung hast du noch nicht bezahlt. Weitere Infos hierzu findest du in deinem Profil. Sobald du bezahlt hast verschwindet auch diese Erinnerung. Danke!<input type="button" value="<?php _e("Später..."); ?>" onclick="jQuery('div#payment_notification').remove();"/></div>
-<?php
-	endif;
-?>
 
 
 <?php
