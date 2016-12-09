8ae870fe582b67439de544319dedf1efa3bc5c3a
Florian Reimair <florian.reimair@itdevas.at>
2016-12-09 17:33:10 +0100

    acf part3 and final

--- wp-content/plugins/advanced-custom-fields/js/input/wysiwyg.js
+++ wp-content/plugins/advanced-custom-fields/js/input/wysiwyg.js
@@ -14,12 +14,12 @@
 	
 	var _wysiwyg = acf.fields.wysiwyg = {
 		
-		$el : null,
-		$textarea : null,
+		$el: null,
+		$textarea: null,
 		
-		o : {},
+		o: {},
 		
-		set : function( o ){
+		set: function( o ){
 			
 			// merge in new option
 			$.extend( this, o );
@@ -38,6 +38,7 @@
 			return this;
 			
 		},
+		
 		has_tinymce : function(){
 		
 			var r = false;
@@ -76,7 +77,8 @@
 			
 			
 			// vars
-			var toolbar = this.get_toolbar(),
+			var id = this.o.id,
+				toolbar = this.get_toolbar(),
 				command = 'mceAddControl',
 				setting = 'theme_advanced_buttons{i}';
 			
@@ -120,15 +122,19 @@
 			
 			
 			// add editor
-			tinyMCE.execCommand( command, false, this.o.id);
+			tinyMCE.execCommand( command, false, id);
 			
 			
 			// events - load
-			$(document).trigger('acf/wysiwyg/load', this.o.id);
+			$(document).trigger('acf/wysiwyg/load', id);
 			
 			
 			// add events (click, focus, blur) for inserting image into correct editor
-			this.add_events();
+			setTimeout(function(){
+				
+				_wysiwyg.add_events( id );
+				
+			}, 100);
 				
 			
 			// restore tinyMCE.settings
@@ -139,18 +145,15 @@
 			wpActiveEditor = null;
 					
 		},
-		add_events : function(){
 		
+		add_events: function( id ){
+			
 			// vars
-			var id = this.o.id,
-				editor = tinyMCE.get( id );
+			var editor = tinyMCE.get( id );
 			
 			
 			// validate
-			if( !editor )
-			{
-				return;
-			}
+			if( !editor ) return;
 			
 			
 			// vars
@@ -518,7 +521,7 @@
 			// Add events to content editor
 			if( wp_content )
 			{
-				_wysiwyg.set({ $el : $('#wp-content-wrap') }).add_events();
+				_wysiwyg.add_events('content');
 			}
 			
 			
