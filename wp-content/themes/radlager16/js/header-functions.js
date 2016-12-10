ecf4caf0a72d814184a9dae309c7d0055bbbfa7d
zaila99 <gernot_winkler@gmx.at>
2016-12-10 18:59:34 +0100

    don't scroll banner

--- wp-content/themes/radlager16/js/header-functions.js
+++ wp-content/themes/radlager16/js/header-functions.js
@@ -1,16 +1,15 @@
 jQuery(window).scroll(function() {
-   
+         
 		 var scrollPos = jQuery(window).scrollTop();
 		
-		if (scrollPos >= 1) {	
-			jQuery(".banner").css({"margin-top":"-337px"});
+		if (scrollPos >= 200) {	
 			jQuery(".RL-Logo_oben").css({"display":"block","opacity":"1"});	
 	}
 	else
 	{
-		jQuery(".banner").css({"margin-top":"75px"});
 		jQuery(".RL-Logo_oben").css({"display":"block","opacity":"0"});
-	}
+
+    };
 	});
 
 
