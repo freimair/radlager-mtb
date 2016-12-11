7db842dd4cf90b497bb6ccf27c9bad1c09f33420
zaila99 <gernot_winkler@gmx.at>
2016-12-11 20:10:48 +0100

    fix password change

--- wp-content/plugins/additional-user-fields/additional-user-fields.php
+++ wp-content/plugins/additional-user-fields/additional-user-fields.php
@@ -182,16 +182,16 @@ $profileuser = get_user_to_edit(get_current_user_id());
 <tr id="password" class="user-pass1-wrap">
 	<th><label for="pass1"><?php _e( 'Neues Passwort' ); ?></label></th>
 	<td>
-		<input class="hidden" value=" " /><!-- #24364 workaround -->
-		<button type="button" class="button button-secondary wp-generate-pw hide-if-no-js"><?php _e( 'Passwort generieren' ); ?></button>
+		<!-- #24364 workaround -->
+		<button type="button" class="button button-secondary wp-generate-pw hide-if-no-js"><?php _e( 'Passwort eingeben' ); ?></button>
 		<div class="wp-pwd hide-if-js">
 			<span class="password-input-wrapper">
-				<input type="password" name="pass1" id="pass1" class="regular-text" value="" autocomplete="off" data-pw="<?php echo esc_attr( wp_generate_password( 24 ) ); ?>" aria-describedby="pass-strength-result" />
+				<input type="password" name="pass1" id="pass1" class="regular-text" value="" autocomplete="off" data-pw="<?php echo esc_attr( wp_generate_password( 0 ) ); ?>" aria-describedby="pass-strength-result" />
 			</span>
-			<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Hide password' ); ?>">
+		<!--	<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Hide password' ); ?>">
 				<span class="dashicons dashicons-hidden"></span>
 				<span class="text"><?php _e( 'Hide' ); ?></span>
-			</button>
+			</button> -->
 			<button type="button" class="button button-secondary wp-cancel-pw hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Cancel password change' ); ?>">
 				<span class="text"><?php _e( 'Cancel' ); ?></span>
 			</button>
