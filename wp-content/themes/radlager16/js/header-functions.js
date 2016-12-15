405569657bdf56f47833742013f3c2266e74bfb4
zaila99 <gernot_winkler@gmx.at>
2016-12-15 21:25:19 +0100

    forgot to commit last time

--- wp-content/themes/radlager16/js/header-functions.js
+++ wp-content/themes/radlager16/js/header-functions.js
@@ -26,3 +26,73 @@ jQuery(window).scroll(function() {
         jQuery("#searchbutton").toggle(0);
         jQuery(".searchmenu").toggle(0);
 	};
+    
+         function userlogin(){
+                  
+         jQuery('.slidewindow').append('ffffffffffffffffffff');	      
+           
+	};
+    
+
+    
+function openslidewindow(fromtop) {
+         
+    
+         jQuery('#content').css({
+						   'position' : 'fixed',
+						   'width' : '100%',
+                           'top' : (fromtop*(-1)), 
+                           });
+    
+         jQuery('.slidewindow').css({
+						   'position' : 'relative',
+						   'width' : '100%',
+						   'z-index' : '2',
+						   'background-color' : 'rgba(0, 0, 0, 0.60)',
+						   'opacity' : '1',
+						   'min-height' : $j('html').height()	
+						   });
+    
+         jQuery('#closearticlebutton').css({
+						   'display' : 'block',
+						   'position' : 'relative',
+						   'z-index' : '200',
+						   });
+    
+         jQuery(document).scrollTop(0);
+         
+
+}
+
+function closeslidewindow(fromtop) {
+         
+         
+         jQuery('#closearticlebutton').add('#closewin').on ('click', function(e) {
+								
+                  jQuery('#closearticlebutton').css({'display' : 'none'});
+                  jQuery('.slidewindow').css({'opacity' : '0', 'min-height' : '0px'});
+						
+                  setTimeout(function(){ 
+                           jQuery('.slidewindow').empty();
+                           jQuery('#content').css({
+                                    'position' : 'relative',
+                                    'top' : '0px',
+						   });
+                  
+                           jQuery(document).scrollTop(fromtop);
+						
+				  },200);					
+         
+         });
+}
+
+
+function pushlogin() {
+         
+         var fromtop = $j(document).scrollTop();
+         
+         openslidewindow(fromtop);
+         jQuery('.login').appendTo ('.slidewindow');
+         jQuery('.login').append('<input id="closewin" type = "button" value="&#xf00d;">');		
+         closeslidewindow(fromtop);
+}
